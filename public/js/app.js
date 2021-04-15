4e18f574e02b20356f0c08fc62b6a136ebaf1618
natof <rallonathanael@gmail.com>
2021-04-15 16:24:28 -0400

    a

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,10 @@ $('.m-nav-toggle').click(function(e){
     $(".m-right").toggleClass("is-open");
     $(".m-nav-toggle").toggleClass("is-open");
 });
+
+const nav = document.querySelector('.skill-image')
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 424){
+        nav.classList.add('scroll');
+    }
+})
